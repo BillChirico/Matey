884cb22653751bdddfc22dc1a03ebe404ad720a8
Bill Chirico <billchirico@gmail.com>
2016-10-07 17:47:53 -0400

    Renamed Property to Premises.

---
+++ src/Matey.Domain/Models/Premises/Premises.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Matey.Domain.Models.Common;
+
+namespace Matey.Domain.Models.Premises
+{
+    public class Premises : Entity<int>
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public List<PremisesMember> Members { get; set; }
+    }
+}
---
+++ src/Matey.Domain/Models/Premises/PremisesMember.cs
@@ -0,0 +1,14 @@
+﻿using Matey.Domain.Models.Common;
+using Matey.Domain.Models.Identity;
+
+namespace Matey.Domain.Models.Premises
+{
+    public class PremisesMember : Entity<int>
+    {
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        public bool Admin { get; set; }
+    }
+}
