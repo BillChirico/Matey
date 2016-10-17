507bfc68bbbeaee849e6d052c52ce86ab63c1353
Bill Chirico <billchirico@gmail.com>
2016-10-17 11:09:57 -0400

    Added ServiceTests class. Added test references.

---
+++ Matey.Tests/ServiceTests/ServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Matey.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matey.Tests.ServiceTests
+{
+    public abstract class ServiceTests
+    {
+        protected readonly DbContextOptions<MateyDbContext> ContextOptions;
+
+        protected ServiceTests()
+        {
+            // Create a service provider to be shared by all test methods
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create options telling the context to use an
+            // InMemory database and the service provider.
+            var builder = new DbContextOptionsBuilder<MateyDbContext>();
+            builder.UseInMemoryDatabase()
+                .UseInternalServiceProvider(serviceProvider);
+
+            ContextOptions = builder.Options;
+        }
+    }
+}
