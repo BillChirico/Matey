a2a808055f2b57888ecc35bedd133df5ff244946
Bill Chirico <billchirico@gmail.com>
2016-11-01 17:09:58 -0400

    Added Utility service tests.

---
+++ test/Matey.Tests/ServiceTests/UtilityTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Matey.Domain;
+using Matey.Domain.Models.Premises;
+using Matey.Domain.Models.Utilities;
+
+namespace Matey.Tests.ServiceTests
+{
+    public class UtilityTests : ServiceTest
+    {
+        public UtilityTests()
+        {
+            using (var context = new MateyDbContext(ContextOptions))
+            {
+                // Add 5 premises
+                for (var i = 1; i <= 5; i++)
+                {
+                    context.Utilities.Add(new Utility
+                    {
+                        Id = i,
+                        Name = "Utility" + i
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
